92991d307a3b1b15faee03997bd8517a565984e3
Tri Nguyen <mankinchi.1995@gmail.com>
2019-11-10 21:23:51 +1100

    complete saving and retriving

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,45 @@ $("#currentDate").text(today.format("DD MMM YYYY"));
 const fromTime = today.clone().hour(9);
 const toTime = today.clone().hour(17);
 
-console.log('From time: ', fromTime.format());
-console.log('To Time: ', toTime.format());
+let planner;
+const LOCAL_STORAGE_KEY = 'planner';
+
+const localStoragePlanner = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (localStoragePlanner !== null) {
+	planner = JSON.parse(localStoragePlanner);
+} else {
+	planner = {};
+}
 
 for (let currentTime = fromTime.clone(); currentTime.isSameOrBefore(toTime); currentTime.add(1, 'h')) {
+	const hour = currentTime.hour();
+
 	// Cloning the clone item
 	const hourSection = $("#cloneItem").clone();
 
 	// Modify the cloned hour section
 	hourSection.removeAttr('id');
-	hourSection.data("hour", currentTime.hour());
+	hourSection.data("hour", hour);
 	hourSection.find(".hour").text(currentTime.format("hA"));
+
+	if (planner[hour] !== undefined) {
+		hourSection.find(".memoContainer").val(planner[hour]);
+	}
 
 	$("#plannerContainer").append(hourSection);
 }
+
+$("#clearPlannerButton").click(function() {
+	localStorage.removeItem(LOCAL_STORAGE_KEY);
+	location.reload();
+});
+
+$("#plannerContainer").on("click", ".saveButton", function() {
+	const $parent = $(this).parent();
+
+	const hour = $parent.data('hour');
+	const userInput = $parent.find(".memoContainer").val();
+
+	planner[hour] = userInput;
+	localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(planner));
+});
