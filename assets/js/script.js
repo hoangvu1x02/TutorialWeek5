d366210c08ab4c2f75ad674a3fa28178aeb8a231
Tri Nguyen <mankinchi.1995@gmail.com>
2019-11-10 20:54:40 +1100

    do cloning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
-const today = moment();
+const today = moment().startOf('hour');
 $("#currentDate").text(today.format("DD MMM YYYY"));
+
+const fromTime = today.clone().hour(9);
+const toTime = today.clone().hour(17);
+
+console.log('From time: ', fromTime.format());
+console.log('To Time: ', toTime.format());
+
+for (let currentTime = fromTime.clone(); currentTime.isSameOrBefore(toTime); currentTime.add(1, 'h')) {
+	// Cloning the clone item
+	const hourSection = $("#cloneItem").clone();
+
+	// Modify the cloned hour section
+	hourSection.removeAttr('id');
+	hourSection.data("hour", currentTime.hour());
+	hourSection.find(".hour").text(currentTime.format("hA"));
+
+	$("#plannerContainer").append(hourSection);
+}
